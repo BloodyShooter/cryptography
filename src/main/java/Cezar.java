e0c0406c8a673f80332786ca380226597b57604f
Егорка <gvozdetscky.egor@yandex.ru>
2019-06-01 22:42:29 +0400

    Цезар

---
+++ src/main/java/Cezar.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cezar {
+
+    private Map<Integer, String> dict = new HashMap<>();
+
+    {
+        dict.put(1, "А");
+        dict.put(1, "Б");
+        dict.put(1, "В");
+        dict.put(1, "Г");
+        dict.put(1, "Д");
+        dict.put(1, "Е");
+        dict.put(1, "Ё");
+        dict.put(1, "Ж");
+        dict.put(1, "З");
+        dict.put(1, "А");
+        dict.put(1, "А");
+        dict.put(1, "А");
+        dict.put(1, "А");
+        dict.put(1, "А");
+        dict.put(1, "А");
+        dict.put(1, "А");
+        dict.put(1, "А");
+        dict.put(1, "А");
+        dict.put(1, "А");
+
+
+    }
+}
