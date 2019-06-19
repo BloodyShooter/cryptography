4ace8db74cf07901065d4c00ccf0d16b94cfbf37
Егорка <gvozdetscky.egor@yandex.ru>
2019-06-20 02:45:44 +0400

    Написал цифр цезаря для зашифровки

--- src/main/java/Cezar.java
+++ src/main/java/Cezar.java
@@ -3,29 +3,75 @@ import java.util.Map;
 
 public class Cezar {
 
-    private Map<Integer, String> dict = new HashMap<>();
+    private Map<Integer, Character> dict = new HashMap<>();
 
     {
-        dict.put(1, "А");
-        dict.put(1, "Б");
-        dict.put(1, "В");
-        dict.put(1, "Г");
-        dict.put(1, "Д");
-        dict.put(1, "Е");
-        dict.put(1, "Ё");
-        dict.put(1, "Ж");
-        dict.put(1, "З");
-        dict.put(1, "А");
-        dict.put(1, "А");
-        dict.put(1, "А");
-        dict.put(1, "А");
-        dict.put(1, "А");
-        dict.put(1, "А");
-        dict.put(1, "А");
-        dict.put(1, "А");
-        dict.put(1, "А");
-        dict.put(1, "А");
+        dict.put(1, 'А');
+        dict.put(2, 'Б');
+        dict.put(3, 'В');
+        dict.put(4, 'Г');
+        dict.put(5, 'Д');
+        dict.put(6, 'Е');
+        dict.put(7, 'Ё');
+        dict.put(8, 'Ж');
+        dict.put(9, 'З');
+        dict.put(10, 'И');
+        dict.put(11, 'Й');
+        dict.put(12, 'К');
+        dict.put(13, 'Л');
+        dict.put(14, 'М');
+        dict.put(15, 'Н');
+        dict.put(16, 'О');
+        dict.put(17, 'П');
+        dict.put(18, 'Р');
+        dict.put(19, 'С');
+        dict.put(20, 'Т');
+        dict.put(21, 'У');
+        dict.put(22, 'Ф');
+        dict.put(23, 'Х');
+        dict.put(24, 'Ц');
+        dict.put(25, 'Ч');
+        dict.put(26, 'Ш');
+        dict.put(27, 'Щ');
+        dict.put(28, 'Ъ');
+        dict.put(29, 'Ы');
+        dict.put(30, 'Ь');
+        dict.put(31, 'Э');
+        dict.put(32, 'Ю');
+        dict.put(33, 'Я');
 
 
+    }
+
+    public String coding(String text, int key) {
+        text = text.toUpperCase();
+        char[] chars = text.toCharArray();
+
+        StringBuffer newStr = new StringBuffer();
+
+        for (char symbol: chars) {
+
+            if (symbol == ' '){
+                newStr.append(" ");
+                continue;
+            }
+
+            dict.forEach((keyMap, value) -> {
+                if (symbol == value) {
+                    if (keyMap + key < dict.size()) {
+                        newStr.append(dict.get(keyMap + key));
+                    } else {
+                        newStr.append(dict.get(keyMap + key - dict.size()));
+                    }
+
+                }
+            });
+        }
+
+        return newStr.toString();
+    }
+
+    public String uncoding(String text, int key) {
+        return null;
     }
 }
---
+++ src/test/java/CezarTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CezarTest {
+
+    Cezar cezar = new Cezar();
+    final String text = "Встретимся завтра утром";
+    final String shifrText = "ЛЫЬЪОЬТЦЫИ СЙЛЬЪЙ ЭЬЪШЦ";
+
+    @Test
+    public void coding() {
+
+
+        String coding = cezar.coding(text, 10);
+
+
+        final String msg = "Неполучилось зашифровать";
+        assertEquals(msg, shifrText, coding);
+    }
+
+    @Test
+    public void uncoding() {
+
+        String coding = cezar.uncoding(shifrText, 10);
+
+
+        final String msg = "Неполучилось зашифровать";
+        assertEquals(msg, text, coding);
+    }
+}
